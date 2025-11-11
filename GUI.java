76f7dfc269675414184398ea7e68380944cc5665
mala9311 <malakumari9311@gmail.com>
2025-11-11 15:01:26 +0530

    added label and buttun in frame using swing

--- GUI.java
+++ GUI.java
@@ -8,5 +8,12 @@ public class GUI {
         frame.setSize(400,400);
          frame.setLayout(null);
          frame.setVisible(true);
+         JLabel l1 = new JLabel("Click the button");
+         l1.setBounds(123,65,100,30);
+         frame.add(l1);
+      //   JButton b1 = new JButton(new ImageIcon("D:\\Java Advance\\image.png"));
+        JButton b1 = new JButton("Click me");
+         b1.setBounds(120,100,100,50);
+         frame.add(b1);
     }
 }

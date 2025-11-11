1d9c24a811e6de450869ffbba52677b72c13d83f
mala9311 <malakumari9311@gmail.com>
2025-11-11 14:11:09 +0530

    create frame by swing

---
+++ GUI.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Main Frame");
+        //set width and height
+        frame.setSize(400,400);
+         frame.setLayout(null);
+         frame.setVisible(true);
+    }
+}

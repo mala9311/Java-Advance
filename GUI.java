7446da5a68622d5b73b119e62c858bbc6156b521
mala9311 <malakumari9311@gmail.com>
2025-11-12 17:39:58 +0530

    GUI

--- GUI.java
+++ GUI.java
@@ -51,9 +51,29 @@ public class GUI {
         // combobox1.setBounds(50,180,100,30);
         // frame.add(combobox1);
        // JOptionPane.showMessageDialog(frame, "Welcome to Hoshiyarpur", "Alert", JOptionPane.WARNING_MESSAGE);
-        String name = JOptionPane.showInputDialog(frame,"Enter your name");
-        System.out.println(name);
+        // String name = JOptionPane.showInputDialog(frame,"Enter your name");
+        // System.out.println(name);
+      JMenuBar mb = new JMenuBar();
+      JMenu menu,submenu;
+      JMenuItem i1,i2,i3,i4,i5;
+      menu = new JMenu("Menu");
+      submenu = new JMenu("Sub Menu");
+      i1 = new JMenuItem("Item 1"); 
+      i2 = new JMenuItem("Item 2"); 
+      i3 = new JMenuItem("Item 3"); 
+      i4 = new JMenuItem("Item 4"); 
+      i5 = new JMenuItem("Item 5");
+      menu.add(i1);
+      menu.add(i2);
+      menu.add(i3);
+     submenu.add(i4);
+      submenu.add(i5);
+      menu.add(submenu);
+      mb.add(menu);
+      frame.setJMenuBar(mb);
 
+
+       
 
 
    }

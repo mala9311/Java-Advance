25af6612868653b8de1b4bbabc95ad75aa267401
mala9311 <malakumari9311@gmail.com>
2026-01-03 14:44:34 +0530

    JDBC MYSQL connection

---
+++ JDBC.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+public class JDBC {
+    public static void main(String[] args) {
+        //Load Driver 2
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        // Create Connection 3
+        String url = "jdbc:mysql://localhost:3306/COLLEGE";
+        String user = "root";
+        String password = "arch@18k";  
+        Connection con = DriverManager.getConnection(url,user,password);
+        //Create statement 4
+        Statement stm = con.createStatement();
+        //Execute Query 5
+        String query = "INSERT INTO STUDENT VALUES(107,'ABC','12th',67,'pass')";
+        stm.executeUpdate(query);
+        // close connection 6
+        con.close();
+        // ##################  TO OUTPUT ON CONSOLE ###############
+            System.out.println("Data Inserted");
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+       
+
+    }
+    
+}

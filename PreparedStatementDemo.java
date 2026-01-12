789345525f96d9c63147424d65105aeeaa6a6bfc
mala9311 <malakumari9311@gmail.com>
2026-01-12 14:46:19 +0530

    prepared statement

--- PreparedStatementDemo.java
+++ PreparedStatementDemo.java
@@ -1,66 +1,34 @@
-// import java.sql.*;
-// public class PreparedStatement {
-//     public static void main(String[] args) {
-//         try {
-//             //Load Driver 2
-//              Class.forName("com.mysql.cj.jdbc.Driver");
-//              //create connection 3
-//         String url = "jdbc:mysql://localhost:3306/temp1";
-//         String Username = "root";
-//         String password ="arch@18k";
-//         Connection con = DriverManager.getConnection(url,Username,password);
-//         // create statement 4
-//         PreparedStatement pstmt = con.prepareStatement("INSERT INTO table1 VALUES(?,?,?)");
-        
-//         //excecute query 5
-//         // pstmt.setInt(1,1111);
-//          pstmt.setString(111,"miky");
-//         pstmt.executeUpdate();
-//         // close connection 6
-//         con.close();
-//         System.out.println("DATA INSERTED");
-            
-//         } catch (Exception e) {
-//             System.out.println(e.getMessage());
-//         }
-       
-
-//     }
-    
-// }
 import java.sql.*;
+import java.util.Scanner;
+public class preparedStatementDemo {
+    public static final String url = "jdbc:mysql://localhost:3306/temp1";
+    public static final String UserName = "root";
+    public static final String Password = "arch@18k";
 
-public class PreparedStatementDemo {
     public static void main(String[] args) {
-        try {
-            // 1. Load Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+        try{
+        //Load Server 2
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Create Connection 3
+        
+        Connection con = DriverManager.getConnection(url,UserName,Password);
+        //Create Statement 4
+        String query = "INSERT INTO table1 VALUES(104,'Raftaar','12th')";
+        PreparedStatement pstmt = con.prepareStatement("INSERT INTO table1 VALUES(?,?)");
 
-            // 2. Create Connection
-            String url = "jdbc:mysql://localhost:3306/temp1";
-            String username = "root";
-            String password = "arch@18k";
-            Connection con = DriverManager.getConnection(url, username, password);
+        //Execute Query 5
+        pstmt.setString(105,"Miky");
+        pstmt.executeUpdate(query);
 
-            // 3. Create PreparedStatement
-            PreparedStatement pstmt =
-                    con.prepareStatement("INSERT INTO table1 VALUES(?,?,?)");
+        // close Connection 6
+        con.close();
+        System.out.println("Data Inserted");
 
-            // 4. Set values
-            pstmt.setInt(1, 111);
-            pstmt.setString(2, "miky");
-            pstmt.setString(3, "11th");
-
-            // 5. Execute
-            pstmt.executeUpdate();
-
-            // 6. Close connection
-            con.close();
-
-            System.out.println("DATA INSERTED SUCCESSFULLY");
-
-        } catch (Exception e) {
-            System.out.println(e);
         }
+        catch(Exception e){
+            System.out.println("Data Inserted");
+        }
+        
     }
+    
 }
